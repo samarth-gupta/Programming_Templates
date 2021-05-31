7be94d7fbfdc0e6c42dcdf2fc0472c841e3876c2
Samarth Gupta <g.samarth98@gmail.com>
2021-06-01 01:15:48 +0530

    added iterative segment tree, fixed tabs and spaces

---
+++ RangeStructures/IterativeSegmentTree.cpp
@@ -0,0 +1,84 @@
+class Node
+{
+public:
+	ll val;
+
+	Node()
+	{
+		val = 0;
+	}
+
+	Node(ll x)
+	{
+		val = x;
+	}
+
+	static Node merge(Node left, Node right)
+	{
+		Node result = Node();
+
+		result.val = left.val + right.val;
+
+		return result;
+	}
+
+};
+
+template<class T>
+class SegmentTree
+{
+public:
+	int N, sz;
+	vector<T>tree;
+
+	SegmentTree( int n )
+	{
+		N = n+5;
+		sz = 2*N;
+		tree.resize(sz,0);
+	}
+
+	SegmentTree( vector<T>&a )
+	{
+		N = a.size();
+		sz = 2*N;
+		for(int i=0;i<N;i++)
+			tree[i+N] = a[i];
+
+		for(int i=N-1;i>0;i--)
+			tree[i] = T::merge( tree[i<<1], tree[i<<1|1] );
+	}
+
+	void set( int pos, T val )
+	{
+		for( tree[pos+=N] = val; pos > 1 ; pos >>= 1 )
+			tree[pos/2] = T::merge( tree[(pos|1)^1], tree[pos|1] );
+	}
+
+	// NOTE BEFORE USING
+	// returns cumulative value of range [l,r)
+	T cumulative( int l, int r )
+	{
+		#warning half open interval
+		T left, right;
+		for( l+=N, r+=N; l < r ; l >>= 1, r >>= 1 )
+		{
+			if( l&1 )
+				left = T::merge( left, tree[l++] );
+			if( r&1 )
+				right = T::merge( tree[--r], right );
+		}
+		return T::merge( left, right );
+	}
+
+	T get( int pos )
+	{
+		return tree[pos+N];
+	}
+
+	void add( int pos, T addend )
+	{
+		set( pos, T::merge( tree[pos+N], addend ) );
+	}
+
+};
--- RangeStructures/SegmentTree1D.cpp
+++ RangeStructures/SegmentTree1D.cpp
@@ -28,95 +28,95 @@ template<class T>
 class SegmentTree
 {
 public:
-    int N, sz;
-    vector<T>tree, actual_value;
+	int N, sz;
+	vector<T>tree, actual_value;
 
-    SegmentTree( int n )
-    {
-        N = n+5;
-        sz = 4*N;
-        tree.resize(sz,0);
-        actual_value.resize(N,0);
-    }
+	SegmentTree( int n )
+	{
+		N = n+5;
+		sz = 4*N;
+		tree.resize(sz,0);
+		actual_value.resize(N,0);
+	}
 
-    SegmentTree( vector<T>&a )
-    {
-    	actual_value.assign(a.begin(), a.end());
-    	SegmentTree(a.size());
-    	build(0,0,N-1);
-    }
+	SegmentTree( vector<T>&a )
+	{
+		actual_value.assign(a.begin(), a.end());
+		SegmentTree(a.size());
+		build(0,0,N-1);
+	}
 
-    void build( int v, int l, int r )
-    {
-        if( l == r )
-        {
-            tree[v] = actual_value[l];
-            return;
-        }
-        int m = l + (r-l)/2;
-        build( 2*v+1, l, m );
-        build( 2*v+2, m+1, r );
-        tree[v] = T::merge( tree[2*v+1], tree[2*v+2] );
-    }
+	void build( int v, int l, int r )
+	{
+		if( l == r )
+		{
+			tree[v] = actual_value[l];
+			return;
+		}
+		int m = l + (r-l)/2;
+		build( 2*v+1, l, m );
+		build( 2*v+2, m+1, r );
+		tree[v] = T::merge( tree[2*v+1], tree[2*v+2] );
+	}
 
-    void set( int v, int l, int r, int p, T val )
-    {
-        if( p < l || p > r ) return;
-        if( l == r )
-        {
-            tree[v] = val;
-            actual_value[l] = val;
-            return;
-        }
-        int m = l + (r-l)/2;
-        set( 2*v+1, l, m, p, val );
-        set( 2*v+2, m+1, r, p, val );
-        tree[v] = T::merge( tree[2*v+1], tree[2*v+2] );
-    }
+	void set( int v, int l, int r, int p, T val )
+	{
+		if( p < l || p > r ) return;
+		if( l == r )
+		{
+			tree[v] = val;
+			actual_value[l] = val;
+			return;
+		}
+		int m = l + (r-l)/2;
+		set( 2*v+1, l, m, p, val );
+		set( 2*v+2, m+1, r, p, val );
+		tree[v] = T::merge( tree[2*v+1], tree[2*v+2] );
+	}
 
-    void add( int v, int l, int r, int p, T addend )
-    {
-    	if( p < l || p > r ) return;
-    	if( l == r )
-    	{
-    		tree[v] = T::merge( tree[v], addend );
-    		actual_value[l] = T::merge( actual_value[l], addend );
-    		return;
-    	}
-    	int m = l + (r-l)/2;
-    	add( 2*v+1, l, m, p, addend );
-    	add( 2*v+2, m+1, r, p, addend );
-    	tree[v] = T::merge( tree[2*v+1], tree[2*v+2] );
-    }
+	void add( int v, int l, int r, int p, T addend )
+	{
+		if( p < l || p > r ) return;
+		if( l == r )
+		{
+			tree[v] = T::merge( tree[v], addend );
+			actual_value[l] = T::merge( actual_value[l], addend );
+			return;
+		}
+		int m = l + (r-l)/2;
+		add( 2*v+1, l, m, p, addend );
+		add( 2*v+2, m+1, r, p, addend );
+		tree[v] = T::merge( tree[2*v+1], tree[2*v+2] );
+	}
 
-    T query( int v, int l, int r, int ql, int qr )
-    {
-        if( qr < l || ql > r )
-            return T();
-        if( ql <= l && r <= qr )
-            return tree[v];
-        int m = l + (r-l)/2;
-        return T::merge( query(2*v+1,l,m,ql,qr), query(2*v+2,m+1,r,ql,qr) );
-    }
+	T query( int v, int l, int r, int ql, int qr )
+	{
+		if( qr < l || ql > r )
+			return T();
+		if( ql <= l && r <= qr )
+			return tree[v];
+		int m = l + (r-l)/2;
+		return T::merge( query(2*v+1,l,m,ql,qr), query(2*v+2,m+1,r,ql,qr) );
+	}
 
-    T get( int pos )
-    {
-    	return actual_value[pos];
-    }
+	T get( int pos )
+	{
+		return actual_value[pos];
+	}
 
-    T get( int l, int r )
-    {
-        return query(0,0,N-1,l,r);
-    }
+	T get( int l, int r )
+	{
+		return query(0,0,N-1,l,r);
+	}
 
-    void set( int pos, T val )
-    {
-    	set(0,0,N-1,pos,val);
-    }
+	void set( int pos, T val )
+	{
+		set(0,0,N-1,pos,val);
+	}
 
-    void add( int pos, T addend )
-    {
-    	add(0,0,N-1,pos,addend);
-    }
+	void add( int pos, T addend )
+	{
+		add(0,0,N-1,pos,addend);
+	}
 
 };
